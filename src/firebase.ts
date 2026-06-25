540a5a4b069ef8743c1af06d5ae7199b00119117
John Rimuel Bigata <57857770+itsmerims@users.noreply.github.com>
2026-06-25 17:09:01 +0800

    Updated firebase connection details

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import config from '../firebase-applet-config.json';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: config.apiKey,
-  authDomain: config.authDomain,
-  projectId: config.projectId,
-  storageBucket: config.storageBucket,
-  messagingSenderId: config.messagingSenderId,
-  appId: config.appId,
-  measurementId: config.measurementId
+  apiKey: "AIzaSyABu2ykOLJz0i42GiIyksgnaEERoqmDdS4",
+  authDomain: "rallyupph.firebaseapp.com",
+  projectId: "rallyupph",
+  storageBucket: "rallyupph.firebasestorage.app",
+  messagingSenderId: "66620157767",
+  appId: "1:66620157767:web:0a40c00bf47b960041e662",
+  measurementId: "G-0X1V2HDDNP"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, config.firestoreDatabaseId);
+export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
+export const db = getFirestore(app);
 export const auth = getAuth(app);
